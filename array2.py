a01a3607ef080ac8daf73e231bd889a13aca74e3
madhulucky35 <kancharlamadhavi15@gmail.com>
2023-01-21 12:23:59 +0530

    first commit

---
+++ array2.py
@@ -0,0 +1,18 @@
+rows=3
+col=3
+arr1=[]
+temp=[]
+for i in range(rows):
+    temp=input("enter ele in rows").split(' ')
+    arr1.append(ele)
+ for j in range(col):
+
+        temp=input("enter ele in rows:").split(' ')
+    arr2.append(ele)
+print(arr2)
+
+res=[[0 for j in range(col)] for i in range(row)]
+for i in range(rows):
+    for j in range(col):
+        res[i][j]
+
---
+++ dictionary1.py
@@ -0,0 +1,26 @@
+print("DICTIONARY")
+details={}
+username1=input("enter 1st username")
+password1=input("enter 1st user password")
+details[username1]=password1
+username2=input("enter 2nd user name")
+password2=input("enter 2nd user password")
+details[username2]=password2
+username3=input("enter 3rd username")
+password3=input("enter 3rd user password")
+details[username3]=password3
+print(details)
+
+print("enter user name:")
+a=input()
+print("password of given username is:",details[a])
+
+l = []
+d = {}
+for i in range(2):
+    d.update({
+        "key1": input("enter key 1:"),
+        "key2": input("enter key 2:")
+    })
+    l.append(d)
+print(d)
